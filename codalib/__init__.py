8acd21f2ef72a5d8a3152031d579a13403a161dd
Damon Kelley <damon.kelley@gmail.com>
2015-06-15 17:18:52 -0500

    Reorganize the package metadata.

--- codalib/__init__.py
+++ codalib/__init__.py
@@ -1,2 +1,11 @@
-APP_AUTHOR = {"name": "Coda",
-              "uri": "http://digital2.library.unt.edu/name/nm0004311/"}
+# codalib Metadata
+
+__title__ = "Coda"
+__uri__ = "http://digital2.library.unt.edu/name/nm0004311/"
+
+__version__ = "0.1.0-dev1"
+
+APP_AUTHOR = {
+    "name": __title__,
+    "uri": __uri__
+}
